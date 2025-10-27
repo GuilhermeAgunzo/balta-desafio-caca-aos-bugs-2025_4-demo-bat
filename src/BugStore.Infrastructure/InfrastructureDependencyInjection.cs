8bc67931ee7faae5b1e7f7a5a95575b5ee963d6f
Guilherme Agunzo <g.agunzo@gmail.com>
2025-10-27 15:22:41 -0300

    feat: configure entities in Infrastructure layer

---
+++ src/BugStore.Infrastructure/InfrastructureDependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using BugStore.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BugStore.Infrastructure;
+
+public static class InfrastructureDependencyInjection
+{
+    public static void AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlite("Data Source=app.db");
+        });
+    }
+}
